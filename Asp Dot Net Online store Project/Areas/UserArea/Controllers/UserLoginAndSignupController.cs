86534d4cdf7f86b8a579541cbece49ad281c3d59
saqibkhan4597 <saqibkhan4597@gmail.com>
2025-10-21 22:41:03 +0500

    Set layout and design and adding LoginAndSignUp controller

---
+++ Asp Dot Net Online store Project/Areas/UserArea/Controllers/UserLoginAndSignupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp_Dot_Net_Online_store_Project.Areas.UserArea.Controllers
+{
+    public class UserLoginAndSignupController : Controller
+    {
+        // GET: UserArea/UserLoginAndSignup
+        public ActionResult SignUpView()
+        {
+            return View();
+        }
+    }
+}
